03d28ddabf994de8feaa8ad9cbd201955ac1b53e
Artur Safonov <artur.safonov23@gmail.com>
2024-01-27 20:12:12 +0200

    init commit

---
+++ src/main/java/com/safonov/betparser/BetParserApplication.java
@@ -0,0 +1,13 @@
+package com.safonov.betparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/safonov/betparser/BetParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safonov.betparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
